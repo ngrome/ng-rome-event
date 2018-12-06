44f9638d47c4f45073a07e4123f5437c07e0f31b
Luciano Murruni <luciano.murruni@conte.it>
2018-12-06 12:52:15 +0100

    public

--- src/app/domain/ng-rome_db/events.ts
+++
@@ -1,9 +0,0 @@
-import { EventsBase } from './base/events.base';
-
-/**
- * YOU CAN OVERRIDE HERE eventsBase.ts
- */
-export interface Events extends EventsBase {
-
-    // Insert here your custom attributes and function
-}
